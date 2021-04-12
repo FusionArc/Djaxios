49128ef854f5ade664dc84a8465d4d613f413016
David Barrow <dave@fusion-arc.uk>
2021-04-13 00:48:55 +0100

    add Product and Category models

--- apps/store/models.py
+++ apps/store/models.py
@@ -4,3 +4,21 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    slug = models.SlugField(max_length=200)
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    price=models.FloatField()
+    def __str__(self):
+        return self.title
