70fbbceebc87a9aaa173eafb39d687c8df30a8da
David Barrow <dave@fusion-arc.uk>
2021-04-13 19:13:24 +0100

    add category detail page and menu links

--- apps/store/views.py
+++ apps/store/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product, Category
 
-def product_detail(request, slug):
+def product_detail(request, category_slug, slug):
     product = get_object_or_404(Product, slug=slug)
 
     context = { 'product': product }
@@ -19,4 +19,4 @@ def category_detail(request, slug):
         'products': products,
     }
 
-    return render(request, 'product_detail.html', context)
+    return render(request, 'category_detail.html', context)
