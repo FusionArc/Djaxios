6e3279f387c07e1f24980f54b8a6e2e362311bb1
David Barrow <dave@fusion-arc.uk>
2021-04-13 23:41:06 +0100

    Updates to stoxk

--- apps/store/models.py
+++ apps/store/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     title = models.CharField(max_length=80)
     subtitle = models.CharField(max_length=200, blank=True)
     description = models.TextField(blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
 
     price=models.DecimalField(decimal_places=2, max_digits=1000)
 
