4df522348f7873e37bc4c051f46ad2215faf29bf
David Barrow <dave@fusion-arc.uk>
2021-04-14 08:19:00 +0100

    Show block title on all pages

--- dj_anxios/urls.py
+++ dj_anxios/urls.py
@@ -1,16 +1,6 @@
-"""
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from apps.cart.views import cart
 from apps.core.views import frontpage, contact, about
 from apps.store.views import product_detail, category_detail
 
@@ -18,6 +8,7 @@ urlpatterns = [
     path('', frontpage, name='frontpage'),
     path('admin/', admin.site.urls),
     path('about/', about, name='about'),
+    path('cart/', cart, name='cart'),
     path('contact/', contact, name='contact'),
     path('<slug:slug>/', category_detail, name='category_detail'),
     path('<slug:category_slug>/<slug:slug>/', product_detail, name='product_detail'),
