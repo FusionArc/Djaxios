b494382618ee95442dfb9312c0c5e83ca30bf761
David Barrow <dave@fusion-arc.uk>
2021-04-14 08:53:19 +0100

    define add and save products i functions in cart

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 
-
 class Cart(object):
     def __init__(self, request):
         self.session = request.session
@@ -10,3 +9,27 @@ class Cart(object):
             cart = self.session[settings.CART_SESSION_ID] = {}
 
         self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        product_id = str(product.id)
+        price = product.price
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                'quantity': 0, 
+                'price': price,
+                'id': product_id
+            }
+            
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += 1
+
+        self.save()
+
+    def save(self):
+        self.session[settings.CART_SESSION_ID] = self.cart
+        self.session.modified = True
+
+
