110f12af6385715c9f178cfe155534371553d9c1
David Barrow <dave@fusion-arc.uk>
2021-04-15 03:45:54 +0100

    updates api

--- apps/store/api.py
+++ apps/store/api.py
@@ -1,14 +1,17 @@
+import json
 from django.http import JsonResponse, request
+from django.shortcuts import get_object_or_404
 
 from apps.cart.cart import Cart
 
 from .models import Product
 
 def api_add_to_cart(request):
+    data = json.loads(request.body)
     jsonresponse = {'success': True}
-    product_id = request.POST.get('product_id')
-    update = request.POST.get('update')
-    quantity = request.POST.get('quantity', 1)
+    product_id = data['product_id']
+    update = data['update']
+    quantity = data['quantity']
 
     cart = Cart(request)
 
@@ -17,6 +20,6 @@ def api_add_to_cart(request):
     if not update:
         cart.add(product=product, quantity=1, update_quantity=False)
     else:
-        cart.add(product=product, quantity=1, update_quantity=True)
+        cart.add(product=product, quantity=quantity, update_quantity=True)
 
     return JsonResponse(jsonresponse)
