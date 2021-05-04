65d6c705d8574c875cb1f82bf4ea049b5018e151
David Barrow <dave@fusion-arc.uk>
2021-05-04 15:19:03 +0100

    breaking changes

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -9,12 +9,19 @@ def cart_detail(request):
     for item in cart:
         product = item['product']
         url = '/%s/%s/' % (product.category.slug, product.slug)
-        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s', 'thumbnail': '%s', 'url': '%s', 'num_available': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'], product.get_thumbnail, url, product.num_available)
+        b = "{'id': '%s', 'title': '%s', 'price': '%s', 'quantity': '%s', 'total_price': '%s', 'url': '%s'}," % (product.id, product.title, product.price, item['quantity'], item['total_price'], url)
 
         productsstring = productsstring + b
 
     context = {
-        'cart': cart
+        'cart': cart,
+        'productsstring': productsstring.rstrip(',')
     }
 
     return render(request, 'cart.html', context)
+
+def success(request):
+    cart = Cart(request)
+    cart.clear()
+    
+    return render(request, 'success.html')
