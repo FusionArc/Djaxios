6f30134b1d7f3d4fbcf025f0f7a4898d35458ad5
David Barrow <dave@fusion-arc.uk>
2021-05-05 01:51:54 +0100

    update image class

--- apps/store/models.py
+++ apps/store/models.py
@@ -15,9 +15,13 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        return '/%s/' % (self.slug)
+
 
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', related_name='variants', on_delete=models.CASCADE, blank=True, null=True)
     slug = models.SlugField(max_length=200)
 
     title = models.CharField(max_length=80)
@@ -36,16 +40,54 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    """def save(self, *args, **kwargs):
+        self.thumbnail = self.make_thumbnail(self.image)
+
+        super().save(*args, **kwargs)"""
+    
+    def get_absolute_url(self):
+        return '/%s/%s/' % (self.category.slug, self.slug)
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return self.thumbnail.url
+        else:
+            if self.image:
+                self.thumbnail = self.make_thumbnail(self.image)
+                self.save()
+                
+                return self.thumbnail.url
+            else:
+                return ''
+    
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        thumbnail = File(thumb_io, name=image.name)
+
+        return thumbnail
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
     def save(self, *args, **kwargs):
-        print('Save', self.image.path)
         self.thumbnail = self.make_thumbnail(self.image)
 
         super().save(*args, **kwargs)
 
-    def make_thumbnail(image, size=(300, 200)):
+    def make_thumbnail(self, image, size=(300, 200)):
         img = Image.open(image)
-        img = Image.convert('RGB')
-        img = Image.thumbnail(size)
+        img.convert('RGB')
+        img.thumbnail(size)
 
         thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
