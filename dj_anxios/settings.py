48fda974b87c3ed5306fd4f53284cf7c0e45b27d
David Barrow <dave@fusion-arc.uk>
2021-05-08 23:32:34 +0100

    create app order

--- dj_anxios/settings.py
+++ dj_anxios/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     
     'apps.cart',
     'apps.core',
+    'apps.order',
     'apps.store'
+
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,8 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = (
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator' },
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator' },
-    { 'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator' },
-    { 'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator' },
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator' },
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator' },
 )
 
 # Internationalization
@@ -95,5 +97,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
